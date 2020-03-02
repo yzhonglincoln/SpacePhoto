82856fabca60780b2c342ded1d1c12c78448a51a
yilinzhong <yzhong@lincolnschool.org>
2020-03-02 10:55:11 -0500

    Initial Commit

---
+++ SpacePhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpacePhoto
+//
+//  Created by Soft Dev on 3/2/20.
+//  Copyright © 2020 Alice Zhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
